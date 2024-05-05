34ae9bcb4e49a25af402ffe11f038b299f272546
Liu Wenyuan <15816141883@163.com>
2024-05-05 11:51:56 +0800

    Add accuracy check to allofthem

--- test_data/allofthem.py
+++ test_data/allofthem.py
@@ -1,6 +1,8 @@
-import vapor_generator.new_vaporizer
+import vapor_generator.new_vaporizer as vaporizer
 import os.path
 import io
+import PIL.ImageChops
+import difflib
 
 mydir = os.path.dirname(os.path.realpath(__file__))
 
@@ -8,8 +10,20 @@ allvapor = []
 with open(os.path.join(mydir, "allofthem.txt"), "r", encoding="utf-8") as all_dat:
     all_dat_l = all_dat.readlines()
     all_dat_l.reverse()
-    allvapor = map(lambda x: x.encode("ascii"), all_dat_l)
+    allvapor = map(lambda x: x.rstrip().encode("ascii"), all_dat_l)
 
 for i, vapor in enumerate(allvapor):
-    with vapor_generator.new_vaporizer.decompress(vapor) as im:
+    with vaporizer.decompress(vapor) as im:
         im.save(os.path.join(mydir, f"allofthem/{i}.png"))
+        recom = vaporizer.compress(im) + b'~~'
+        if recom == vapor:
+            continue
+        print(f"{i} is not the same")
+        print([li for li in difflib.ndiff(vapor.decode("utf-8"), recom.decode("utf-8")) if not li.startswith(" ")])
+        with vaporizer.decompress(recom) as imx:
+            imx.save(os.path.join(mydir, f"allofthem/{i}_r.png"))
+            with PIL.ImageChops.difference(im, imx) as imd:
+                if imd.getbbox():
+                    imd.save(os.path.join(mydir, f"allofthem/{i}_d.png"))
+                else:
+                    print(f"but pixel-perfect according to our decompressor")
