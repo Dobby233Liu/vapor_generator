704737e98a87671201fab50c4e51d8cf3e0cde57
Liu Wenyuan <15816141883@163.com>
2024-05-05 13:11:06 +0800

    Make allofthem write results

--- tests/allofthem.py
+++ tests/allofthem.py
@@ -3,6 +3,7 @@ import os.path
 import io
 import PIL.ImageChops
 import difflib
+import shutil
 
 mydir = os.path.dirname(os.path.realpath(__file__))
 
@@ -12,19 +13,26 @@ with open(os.path.join(mydir, "allofthem.txt"), "r", encoding="utf-8") as all_da
     all_dat_l.reverse()
     allvapor = map(lambda x: x.rstrip().encode("ascii"), all_dat_l)
 
+shutil.rmtree(os.path.join(mydir, "allofthem"), ignore_errors=True)
+os.mkdir(os.path.join(mydir, "allofthem"))
+
 for i, vapor in enumerate(allvapor):
+    with open(os.path.join(mydir, f"allofthem/{i}.bin"), "wb") as f:
+        f.write(vapor)
     with vaporizer.decompress(vapor) as im:
         im.save(os.path.join(mydir, f"allofthem/{i}.png"))
         recom = vaporizer.compress(im) + b'~~'
         if recom == vapor:
             continue
         print(f"{i} is not the same")
-        print([li for li in difflib.ndiff(vapor.decode("utf-8"), recom.decode("utf-8")) if not li.startswith(" ")])
-        print(len(vapor), len(recom))
+        #print([li for li in difflib.ndiff(vapor.decode("utf-8"), recom.decode("utf-8")) if not li.startswith(" ")])
+        #print(len(vapor), len(recom))
+        with open(os.path.join(mydir, f"allofthem/{i}_r.bin"), "wb") as f:
+            f.write(recom)
         with vaporizer.decompress(recom) as imx:
             imx.save(os.path.join(mydir, f"allofthem/{i}_r.png"))
             with PIL.ImageChops.difference(im, imx) as imd:
                 if imd.getbbox():
                     imd.save(os.path.join(mydir, f"allofthem/{i}_d.png"))
                 else:
-                    print(f"but pixel-perfect according to our decompressor")
+                    print(f"pixel-perfect according to our decompressor")
